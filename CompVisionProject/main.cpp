63afb907bf05600c45036cedeb5c573b887abf04
pshekhter <pshekhter@gmail.com>
2017-06-20 14:14:29 -0400

    Set reading from image and displaying.

--- CompVisionProject/main.cpp
+++ CompVisionProject/main.cpp
@@ -5,39 +5,36 @@
 	- Pavel Shekhter
 */
 
+#include <opencv2/core/core.hpp>
 #include <opencv2/opencv.hpp>
 #include <opencv_modules.hpp>
+#include <opencv2/highgui.hpp>
 #include <string>
 #include <fstream>
+#include <thread>
+#include "main.h"
 
-struct VIDEODATA {
-	cv::VideoCapture vc;
-	cv::Size s;
-} vd;
+struct IMAGEDATA {
+	cv::Mat currentFrameColor;
+	cv::Mat currentFrameGry;
+	cv::Mat cannyGaussianDetectedEdges;
+	int lowThresh;
+	int ratio = 3;
+	int kernel = 3;
+} id;
 
-cv::Mat currentFrameColor;
-cv::Mat currentFrameGry;
 
 
 /*
 	Reads the video file into the frame buffer.
 	- Pavel Shekhter
 */
- VIDEODATA readVideoData() {
+ IMAGEDATA readImageData(std::string imagefile) {
 
-	cv::VideoCapture capture(0);
-	if (!capture.isOpened()) {
-		std::cout << "Failed to open webcam!" << std::endl;
-	}
-	double fps = capture.get(cv::CAP_PROP_FPS);
-	cv::Size size(
-		(int)capture.get(cv::CAP_PROP_FRAME_WIDTH),
-		(int)capture.get(cv::CAP_PROP_FRAME_HEIGHT));
-	VIDEODATA vd;
-	vd.vc = capture;
-	vd.s = size;
+	 cv::Mat image = cv::imread(imagefile, CV_LOAD_IMAGE_COLOR);
+	 id.currentFrameColor = image;
 
-	return vd;
+	return id;
 }
 
  /*
@@ -50,11 +47,73 @@ cv::Mat currentFrameGry;
 	 file << "\n\n";
  }
 
-int main(int argc, char** argv) {
+ /*
+	Appends an error message to the report.
+	- Pavel Shekhter
+ */
+ void appendErrorMessage(std::ostream &file, int errorCode) {
+	 file << "There was an error in processing. Error Code: " << errorCode << std::endl;
+	 file << "This means: ";
+	 switch (errorCode) {
+		 case -1: {
+			file << "Error loading file." << std::endl;
+			break;
+		}
+		 case -2: {
+			 file << "Improper loading." << std::endl;
+			 break;
+		 }
+	 }
+ }
+
+/*
+	Parses the arguments from the command line.
+	- Pavel Shekhter
+*/
+ int parseArguments(int argc, char ** argv, std::ofstream &file, bool &retflag)
+ {
+	 retflag = true;
+	 std::string keys = "{@image         |          | image to scan}";
+
+	 cv::CommandLineParser parser(argc, argv, keys);
+	 std::string imagefile = parser.get<std::string>("@image");
+
+	 if (!imagefile.empty()) {
+		 IMAGEDATA id = readImageData(imagefile);
+
+		 if (!id.currentFrameColor.data) {
+			 std::cout << "Can't open file!" << std::endl;
+			 appendErrorMessage(file, -1);
+			 return -1;
+		 }
+	 }
+	 retflag = false;
+	 return {};
+ }
+
+ /*
+	Performs a Canny edge detector using Gaussian blur.
+	- Pavel Shekhter
+ */
+ cv::Mat gaussianCanny() {
+	 cv::blur(id.currentFrameGry, id.currentFrameGry, cv::Size(3, 3));
+	 cv::Canny(id.cannyGaussianDetectedEdges, id.cannyGaussianDetectedEdges, id.lowThresh, id.lowThresh * id.ratio, id.kernel);
+	 cv::Mat dst;
+	 dst = cv::Scalar::all(0);
+	 id.currentFrameColor.copyTo(dst, id.cannyGaussianDetectedEdges);
+	 return dst;
+}
+
+ int main(int argc, char** argv) {
+	 std::ofstream file;
+
+	if (argc != 2) {
+		std::cout << "Usage: CompVisionProject imageToLoad" << std::endl;
+		appendErrorMessage(std::cout, -2);
+		return -2;
+	}
 
 	std::string report;
-
-	std::ofstream file;
 	
 	std::cout << "Enter report file name: " << std::endl;
 	getline(std::cin, report);
@@ -63,7 +122,19 @@ int main(int argc, char** argv) {
 
 	setUpFile(file, report);
 
-	VIDEODATA vd = readVideoData();
+	bool retflag;
+	int retval = parseArguments(argc, argv, file, retflag);
+	if (retflag) return retval;
+
+	cv::namedWindow("Computer Vision Demo", CV_WINDOW_AUTOSIZE);
+	cv::imshow("Computer Vision Demo", id.currentFrameColor);
+	cv::cvtColor(id.currentFrameColor, id.currentFrameGry, cv::COLOR_BGR2GRAY);
+
+	cv::Mat det = gaussianCanny();
+	cv::namedWindow("Gaussian", 1);
+	cv::imshow("Gaussian", det);
+
+	cv::waitKey(0);
 
 	file.close();
 
---
+++ CompVisionProject/main.h
@@ -0,0 +1,3 @@
+#pragma once
+
+int parseArguments(int argc, char ** argv, std::ofstream &file, bool &retflag);
