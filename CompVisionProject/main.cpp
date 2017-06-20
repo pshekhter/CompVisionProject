70133796cad497f321315da691cc6a1a7e3e58fc
pshekhter <pshekhter@gmail.com>
2017-06-20 09:16:28 -0400

    Created code to set up report and read data from webcam.

--- CompVisionProject/main.cpp
+++ CompVisionProject/main.cpp
@@ -8,53 +8,63 @@
 #include <opencv2/opencv.hpp>
 #include <opencv_modules.hpp>
 #include <string>
+#include <fstream>
 
 struct VIDEODATA {
 	cv::VideoCapture vc;
 	cv::Size s;
-	cv::VideoWriter w;
 } vd;
 
 cv::Mat currentFrameColor;
 cv::Mat currentFrameGry;
+std::string report;
 
-/*
-	Moves on to the next frame.
-	- Pavel Shekhter
-*/
-void on_trackbar(int, void*) {
-
-}
 
 /*
 	Reads the video file into the frame buffer.
 	- Pavel Shekhter
 */
-VIDEODATA readVideoFile(std::string filename) {
+ VIDEODATA readVideoData() {
 
-	cv::VideoCapture capture(filename);
+	cv::VideoCapture capture(0);
+	if (!capture.isOpened()) {
+		std::cout << "Failed to open webcam!" << std::endl;
+	}
 	double fps = capture.get(cv::CAP_PROP_FPS);
 	cv::Size size(
 		(int)capture.get(cv::CAP_PROP_FRAME_WIDTH),
 		(int)capture.get(cv::CAP_PROP_FRAME_HEIGHT));
-	cv::VideoWriter writer;
-	writer.open(filename, CV_FOURCC('M', 'J', 'P', 'G'), fps, size);
 	VIDEODATA vd;
 	vd.vc = capture;
 	vd.s = size;
-	vd.w = writer;
-	return vd;
 
+	return vd;
 }
+
+ /*
+	Sets up the report.
+	- Pavel Shekhter
+ */
+ void setUpFile(std::ofstream &file, std::string &report_name) {
+	 file << "Edge Detection Analysis Data: " << std::endl;
+	 file << "Report file name: " << report_name << std::endl;
+	 file << "\n\n";
+ }
 
 int main(int argc, char** argv) {
 
-	std::string filename;
+	std::ofstream file;
 	
-	std::cout << "Enter video file name: " << std::endl;
-	std::cin >> filename;
+	std::cout << "Enter report file name: " << std::endl;
+	getline(std::cin, report);
 
-	VIDEODATA vd = readVideoFile(filename);
+	file.open(report);
+
+	setUpFile(file, report);
+
+	VIDEODATA vd = readVideoData();
+
+	file.close();
 
 	return 0;
 }
