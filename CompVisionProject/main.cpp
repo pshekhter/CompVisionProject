7e2f635a930a38710c9e737e06c62ab3865f1d4b
pshekhter <pshekhter@gmail.com>
2017-06-20 08:19:27 -0400

    Created function to read video file into memory.

--- CompVisionProject/main.cpp
+++ CompVisionProject/main.cpp
@@ -1,28 +1,60 @@
+/*
+	This project is being developed for the CMSC-498 - Senior Seminar Fall 2017 semester at Chestnut Hill College in Philadelphia, PA.
+	The purpose is to test the accuracy and efficiency of the Canny edge detector given different filters, using the Laplacian of Gaussian and Hough
+	pseudo-wavelet as benchmarks.
+	- Pavel Shekhter
+*/
+
 #include <opencv2/opencv.hpp>
 #include <opencv_modules.hpp>
+#include <string>
+
+struct VIDEODATA {
+	cv::VideoCapture vc;
+	cv::Size s;
+	cv::VideoWriter w;
+} vd;
+
+cv::Mat currentFrameColor;
+cv::Mat currentFrameGry;
+
+/*
+	Moves on to the next frame.
+	- Pavel Shekhter
+*/
+void on_trackbar(int, void*) {
+
+}
+
+/*
+	Reads the video file into the frame buffer.
+	- Pavel Shekhter
+*/
+VIDEODATA readVideoFile(std::string filename) {
+
+	cv::VideoCapture capture(filename);
+	double fps = capture.get(cv::CAP_PROP_FPS);
+	cv::Size size(
+		(int)capture.get(cv::CAP_PROP_FRAME_WIDTH),
+		(int)capture.get(cv::CAP_PROP_FRAME_HEIGHT));
+	cv::VideoWriter writer;
+	writer.open(filename, CV_FOURCC('M', 'J', 'P', 'G'), fps, size);
+	VIDEODATA vd;
+	vd.vc = capture;
+	vd.s = size;
+	vd.w = writer;
+	return vd;
+
+}
 
 int main(int argc, char** argv) {
+
+	std::string filename;
 	
-	cv::VideoCapture cap(0); // open default camera
-	if (!cap.isOpened()) {
-		return -1;
-	}
+	std::cout << "Enter video file name: " << std::endl;
+	std::cin >> filename;
 
-	cv::Mat original, edges, gray;
+	VIDEODATA vd = readVideoFile(filename);
 
-	cv::namedWindow("Original", cv::WINDOW_AUTOSIZE);
-	cv::namedWindow("Edges", cv::WINDOW_AUTOSIZE);
-
-	for (;;) {
-		cap >> original; // Get new frame from camera
-		cv::imshow("Original", original);
-		cv::cvtColor(original, gray, cv::COLOR_BGR2GRAY);
-		cv::Scharr(gray, edges, -1, 1, 0, 0, 0, 4);
-		cv::Canny(gray, edges, 35, 35 * 2);
-		cv::imshow("Edges", edges);
-		char c = cv::waitKey(10);
-		if (c == 27) break;
-	}
-	cap.release();
 	return 0;
 }
